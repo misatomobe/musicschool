601974cf61d8e1a7ca95498b5cacbfc031892d27
misatomobe <marimekko.0219@gmail.com>
2026-05-13 21:23:29 +0900

    voice

--- js/script.js
+++ js/script.js
@@ -11,3 +11,25 @@ document.addEventListener('DOMContentLoaded', () => {
     spNav.classList.toggle('active');
   });
 });
+
+// ==============================
+// Swiper
+// ==============================
+const swiper = new Swiper(".swiper", {
+  loop: true, // ループ
+  speed: 1500, // 少しゆっくり(デフォルトは300)
+  autoplay: { // 自動再生
+    delay: 1000, // 1秒後に次のスライド
+    disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
+  },
+  // ページネーション
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // 前後の矢印
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
